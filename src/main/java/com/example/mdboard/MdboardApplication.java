88535bdc60f138c473f8cd54416ae2bac3ac060a
Seungil Kim <ksi990302@gmail.com>
2022-10-30 14:34:33 +0900

    Initial commit

---
+++ src/main/java/com/example/mdboard/MdboardApplication.java
@@ -0,0 +1,13 @@
+package com.example.mdboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MdboardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mdboard/MdboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mdboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
