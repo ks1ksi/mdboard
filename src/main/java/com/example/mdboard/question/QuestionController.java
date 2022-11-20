17667feb65191f90342ca3c7ff204f8077629533
Seungil Kim <ksi990302@gmail.com>
2022-11-20 13:10:29 +0900

    add delete

--- src/main/java/com/example/mdboard/question/QuestionController.java
+++ src/main/java/com/example/mdboard/question/QuestionController.java
@@ -81,4 +81,15 @@ public class QuestionController {
         return "redirect:/question/detail/" + id;
     }
 
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/delete/{id}")
+    public String deleteQuestion(Principal principal, @PathVariable("id") Long id) {
+        Question question = questionService.getQuestion(id);
+        if (!question.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "삭제 권한이 없습니다.");
+        }
+        questionService.delete(question);
+        return "redirect:/";
+    }
+
 }
