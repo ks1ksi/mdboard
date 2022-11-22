66d94e96b82cfa5f640b671b1e6c715f4262eb19
Seungil Kim <ksi990302@gmail.com>
2022-11-22 15:09:57 +0900

    add search

--- src/main/java/com/example/mdboard/question/QuestionRepository.java
+++ src/main/java/com/example/mdboard/question/QuestionRepository.java
@@ -2,6 +2,7 @@ package com.example.mdboard.question;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -13,6 +14,7 @@ public interface QuestionRepository extends JpaRepository<Question, Long> {
 
     List<Question> findBySubjectLike(String subject);
 
-    @Override
     Page<Question> findAll(Pageable pageable);
+
+    Page<Question> findAll(Specification<Question> spec, Pageable pageable);
 }
--- src/main/java/com/example/mdboard/question/QuestionService.java
+++ src/main/java/com/example/mdboard/question/QuestionService.java
@@ -1,5 +1,6 @@
 package com.example.mdboard.question;
 
+import com.example.mdboard.answer.Answer;
 import com.example.mdboard.ex.DataNotFoundException;
 import com.example.mdboard.user.SiteUser;
 import lombok.RequiredArgsConstructor;
@@ -7,8 +8,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.criteria.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -29,11 +32,12 @@ public class QuestionService {
         return oq.get();
     }
 
-    public Page<Question> getList(int page) {
+    public Page<Question> getList(int page, String keyword) {
         List<Sort.Order> sorts = new ArrayList<>();
         sorts.add(Sort.Order.desc("createDate"));
         Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
-        return questionRepository.findAll(pageable);
+        Specification<Question> spec = search(keyword);
+        return questionRepository.findAll(spec, pageable);
     }
 
     public Question create(String subject, String content, SiteUser author) {
@@ -60,5 +64,24 @@ public class QuestionService {
     public void vote(Question question, SiteUser siteUser) {
         question.getVoter().add(siteUser);
         questionRepository.save(question);
+    }
+
+    private Specification<Question> search(String keyword) {
+        return new Specification<Question>() {
+            private static final long serialVersionUID = 1L;
+            @Override
+            public Predicate toPredicate(Root<Question> q, CriteriaQuery<?> query, CriteriaBuilder cb) {
+                query.distinct(true);
+                Join<Question, SiteUser> u1 = q.join("author", JoinType.LEFT);
+                Join<Question, Answer> a = q.join("answerList", JoinType.LEFT);
+                Join<Question, SiteUser> u2 = a.join("author", JoinType.LEFT);
+                return cb.or(
+                        cb.like(q.get("subject"), "%" + keyword + "%"),
+                        cb.like(q.get("content"), "%" + keyword + "%"),
+                        cb.like(u1.get("username"), "%" + keyword + "%"),
+                        cb.like(a.get("content"), "%" + keyword + "%"),
+                        cb.like(u2.get("username"), "%" + keyword + "%"));
+            }
+        };
     }
 }
